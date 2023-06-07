96c0a75278693b33568d2a649860a43bb34a38a7
Georgi Ivanov <gogbg@outlook.com>
2023-06-07 11:59:22 +0300

    Update Get-CommandUsage.ps1

--- Get-CommandUsage.ps1
+++ Get-CommandUsage.ps1
@@ -1,56 +1,49 @@
 [CmdletBinding()]
 param
 (
-    [Parameter(Mandatory, ValueFromPipeline)]
-    [System.IO.FileInfo[]]$File
+  [Parameter(Mandatory, ValueFromPipeline)]
+  [System.IO.FileInfo[]]$File
 )
-begin
-{
-    class commandUsage
-    {
-        [string]$Name
-        [string]$Type
-        [string]$Source
-        [int]$Count = 0
-    }
-    $result = [System.Collections.Generic.Dictionary[string, commandUsage]]::new()
+begin {
+  class commandUsage {
+    [string]$Name
+    [string]$Type
+    [string]$Source
+    [int]$Count = 0
+    [System.Collections.Generic.List[string]]$Usages = [System.Collections.Generic.List[string]]::new()
+  }
+  $result = [System.Collections.Generic.Dictionary[string, commandUsage]]::new()
 }
-process
-{
-    foreach ($f in $File)
-    {
-        $script = Get-Command -Name $f.FullName
-        Get-AstStatement -Ast $script.ScriptBlock.Ast -Type CommandAst | ForEach-Object -Process {
-            $commandName = $_.GetCommandName()
-            if ($result.ContainsKey($commandName))
-            {
-                $result[$commandName].Count++
-            }
-            else
-            {
-                $commandUsage = [commandUsage]::new()
-                $commandUsage.Name = $commandName
-                $commandUsage.Count++
+process {
+  foreach ($f in $File) {
+    $script = Get-Command -Name $f.FullName
+    Get-AstStatement -Ast $script.ScriptBlock.Ast -Type CommandAst | ForEach-Object -Process {
+      $commandName = $_.CommandElements[0].ToString()
+      if ($result.ContainsKey($commandName)) {
+        $result[$commandName].Count++
+        $result[$commandName].Usages.Add("At line: $($_.CommandElements[0].Extent.StartLineNumber)/column: $($_.CommandElements[0].Extent.StartColumnNumber)")
+      } else {
+        $commandUsage = [commandUsage]::new()
+        $commandUsage.Name = $commandName
+        $commandUsage.Usages.Add("At line: $($_.CommandElements[0].Extent.StartLineNumber)/column: $($_.CommandElements[0].Extent.StartColumnNumber)")
+        $commandUsage.Count++
 
-                #Resolve source
-                try
-                {
-                    $cmdFound = Get-Command -Name $commandName -ErrorAction Stop
-                    $commandUsage.Type = $cmdFound.CommandType
-                    $commandUsage.Source = $cmdFound.Source
-                }
-                catch
-                {
-                    $commandUsage.Source = 'Unknown'
-                }
-
-                #Append to result
-                $result.Add($commandName,$commandUsage)
-            }
+        #Resolve source
+        try {
+          $cmdFound = Get-Command -Name $commandName -ErrorAction Stop
+          $commandUsage.Type = $cmdFound.CommandType
+          $commandUsage.Source = $cmdFound.Source
+        } catch {
+          $commandUsage.Type = 'Unknown'
+          $commandUsage.Source = 'Unknown'
         }
+
+        #Append to result
+        $result.Add($commandName, $commandUsage)
+      }
     }
+  }
 }
-end
-{
-    $result.Values
+end {
+  $result.Values
 }
